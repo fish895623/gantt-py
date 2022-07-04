fde91dfe9235fec4cc8ea20e87ac3a06dae5131d
fish895623 <dan990429@gmail.com>
2022-07-04 18:19:27 +0900

    Create class

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
 from datetime import date
 import gantt
 
+
+class Hello:
+    def __init__(
+        self,
+        project_name: str,
+        task_name: str,
+        task_color: str,
+        start_date: date,
+        task_duration: int,
+        resources: list,
+    ):
+        self.task_name = task_name
+        self.start_date = start_date
+        self.task_duration = task_duration
+        self.project_name = project_name
+        self.task_color = task_color
+        self.resources = resources
+
+        self.resource_lists = []
+        self.tasks = []
+        pass
+
+    def Task(self):
+        gantt.Task(
+            name=self.task_name,
+            start=self.start_date,
+            duration=self.task_duration,
+            resources=self.resources,
+        )
+        pass
+
+
 emp = ["Ben", "Alex"]
 
 Ben = gantt.Resource("Ben")
@@ -24,25 +56,6 @@ task4_project1 = gantt.Task(
 )
 
 project_1 = gantt.Project(name="Project 1")
-
-
-class Hello:
-    def __init__(
-        self,
-        project_name: str,
-        task_name: str,
-        task_color: str,
-        start_date: date,
-        task_duration: int,
-        resources: list,
-    ):
-        self.task_name = task_name
-        self.start_date = start_date
-        self.task_duration = task_duration
-        pass
-
-    def Task(self):
-        pass
 
 
 for task in [task1_project1, task2_project1, task3_project1, task4_project1]:
