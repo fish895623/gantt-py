3712d5c404762fe7b371672e42f3821f81ceecbd
fish895623 <dan990429@gmail.com>
2022-07-04 18:07:58 +0900

    First Commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from datetime import date
+import gantt
+
+emp = ["Ben", "Alex"]
+
+Ben = gantt.Resource("Ben")
+Alex = gantt.Resource("Alex")
+
+task1_project1 = gantt.Task(
+    name="task1", start=date(2021, 1, 27), duration=13, resources=[Ben], color="#a3ddcb"
+)
+task2_project1 = gantt.Task(
+    name="task2", start=date(2021, 2, 10), duration=8, resources=[Alex], color="#a3ddcb"
+)
+task3_project1 = gantt.Task(
+    name="task3", start=date(2021, 2, 19), duration=10, resources=[Ben], color="#a3ddcb"
+)
+task4_project1 = gantt.Task(
+    name="task4",
+    start=date(2021, 3, 1),
+    duration=12,
+    resources=[Ben, Alex],
+    color="#a3ddcb",
+)
+
+project_1 = gantt.Project(name="Project 1")
+
+
+class Hello:
+    def __init__(
+        self,
+        project_name: str,
+        task_name: str,
+        task_color: str,
+        start_date: date,
+        task_duration: int,
+        resources: list,
+    ):
+        self.task_name = task_name
+        self.start_date = start_date
+        self.task_duration = task_duration
+        pass
+
+    def Task(self):
+        pass
+
+
+for task in [task1_project1, task2_project1, task3_project1, task4_project1]:
+    project_1.add_task(task)
+
+project_1.make_svg_for_resources(
+    filename="hello.html",
+    today=date(2021, 1, 27),
+    start=date(2021, 1, 20),
+    end=date(2021, 4, 1),
+)
